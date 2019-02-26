7b36c21ee96b85f8d53f29d1c0f3058b6511e0f9
shusant <shusrulz@gmail.com>
2019-02-26 21:45:00 +0545

    second commit

--- app.py
+++ app.py
@@ -3,47 +3,42 @@ from io import BytesIO
 import json
 
 import base64
-
-import cv2
 import numpy as np
+import cv2
 from PIL import Image
-from bokeh import model
+
+import tensorflow as tf
+from keras.models import load_model
 
 from flask import Flask, render_template, request
-from keras.engine.saving import load_model
-from numpy.core.tests.test_einsum import chars
 
 app = Flask(__name__)
 
+graph = tf.get_default_graph()
 
 @app.route("/", methods=['GET', 'POST'])
 def pati():
+    prediction = ''
     if 'img' in request.form.keys():
         s = request.form['img']
         im = Image.open(BytesIO(base64.b64decode(s)))
-        # a = np.array(im)
-        # print(a.shape)
         open_cv_image = np.array(im)
-        open_cv_image = open_cv_image[:, :, ::-1].copy()
+        open_cv_image = open_cv_image[:, :, ::-1].copy() 
         img = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)
         img_resized = cv2.resize(img, (32, 32))
-        cv2.imwrite('last.png', img_resized)
-        input_im = np.reshape(img_resized, (1, 32, 32, 1)) # Passing 1 image through singel channel
-        print(input_im.shape)
-        '''
+        input_im = np.reshape(img_resized, (1, 32, 32, 1))
         with graph.as_default():
             probs = model.predict(input_im)[0]
-            prob_ind = np.argmax(probs) # max value index
+            prob_ind = np.argmax(probs)
             print(probs)
             print(prob_ind)
             prediction = chars[prob_ind]
-        '''
-    return render_template('index.html', prediction ='predicition')
+    return render_template('index.html', prediction=prediction)
 
 
 if __name__ == '__main__':
     model = load_model('out/model.h5')
-    # load devnagari characters
-    with open("data/external/dev-char.txt", encoding ='utf-8') as f:
+    # Load devanagari characters
+    with open("data/external/dev-char.txt", encoding='utf-8') as f:
         chars = f.readline().split(',')
     app.run(debug=True)
